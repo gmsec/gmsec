71d9017e1d77b0859021059e19978c95c47522f4
zanshichun <zsc9602@163.com>
2021-06-23 15:55:02 +0800

    fix:修改压缩文件格式

--- proto_install_windows_git_bash.sh
+++ proto_install_windows_git_bash.sh
@@ -7,7 +7,7 @@ INTERNAL_URL="https://github.com.cnpmjs.org/protocolbuffers/protobuf/releases/do
 
 # su - xxj -c "qwer"
 # download
-curl -fLo protobuf.tar.zip ${ABROAD_URL} || curl -fLo protobuf.tar.zip ${INTERNAL_URL}
+curl -fLo protobuf.tar.gz ${ABROAD_URL} || curl -fLo protobuf.tar.zip ${INTERNAL_URL}
 mkdir protobuf-${version}
 unzip -d ./protobuf-${version}/ protobuf.tar.gz
 cd protobuf-${version}
